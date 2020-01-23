069aa596c3ffdf3f0e729ca9f3a9022f32b4d586
svennela <sridhar.vennela@gmail.com>
2020-01-22 16:33:32 -0800

    Added optional value in hcl element

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"path/filepath"
 	"strings"
@@ -17,12 +18,38 @@ type Variable struct {
 	Default     *string `hcl:"default,attr"`
 }
 
+type Sample struct {
+	A string   `hcl:"a"`
+	B []string `hcl:"b1,optional"`
+}
+
 type Config struct {
 	Variables []Variable `hcl:"variable,block"`
 	Remain    hcl.Body   `hcl:",remain"`
 }
 
 func main() {
+	// processtffiles()
+	parsehclfile("test.hcl")
+}
+
+func parsehclfile(filename string) {
+	fmt.Println("Processing " + filename + " file")
+	parser := hclparse.NewParser()
+	f, parseDiags := parser.ParseHCLFile("test.hcl")
+	if parseDiags.HasErrors() {
+		log.Fatal(parseDiags.Error())
+	}
+
+	var sampleInstance Sample
+	decodeDiags := gohcl.DecodeBody(f.Body, nil, &sampleInstance)
+	if decodeDiags.HasErrors() {
+		log.Fatal(decodeDiags.Error())
+	}
+
+	fmt.Printf("%#v", sampleInstance)
+}
+func processtffiles() {
 	var conf Config
 	var vars []Variable
 
@@ -63,5 +90,4 @@ func main() {
 	for i := 0; i < len(vars); i++ {
 		fmt.Println(*vars[i].Description)
 	}
-
 }
